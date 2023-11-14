ef8fccd6faa03efaee5b22e5a2ab8b6a6cd55ebe
OSSELINAlexandre <alexandre.osselin@gmail.com>
2023-11-14 22:13:28 +0100

    [SimpleFunc] : Making the power 2 work.

--- power.s
+++ power.s
@@ -2,36 +2,34 @@
 .global _start
 
 _start:
-	pushq %rbp
 	movq  %rsp, %rbp
 	
-	pushq $7
-	pushq $2
+	pushq $5
+	pushq $3
 	
 	callq simple_loop
-
+	
 .exit_system:
 	movq $60, %rax
-	movq %rax, %rdi
+	movq %r11, %rdi
 	syscall
 
 .type simple_loop, @function
 simple_loop:
-variables_init:
 	pushq %rbp
 	movq  %rsp, %rbp
-	movq  -24(%rbp), %rdi
-	movq  -16(%rbp), %rcx
-	movq  %rcx, %rsi
+	movq  $1,   %rax
+	movq  24(%rbp), %rdi
+	movq  16(%rbp), %rcx
 
 loop:
-	imulq %rcx, %rdi
-	decq  %rsi
-	cmpq $0, %rsi
-	jne loop
+	imulq %rdi, %rax
+	decq  %rcx
+	cmpq  $0, %rcx
+	jne   loop
 
 quit_loop:
-	movq %rdi, %rax
+	movq %rax, %r11
 	movq %rbp, %rsp
 	popq %rbp
 	ret
